28be7c734cf1b1e13e19e07888f77c5cdc037d8d
Chupi <cmera001@fiu.edu>
2019-01-14 20:39:24 -0500

    fetching server data

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -19,23 +19,34 @@ const FEED_QUERY = gql`
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎',
-        url: 'https://www.prisma.io',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
+    // const linksToRender = [
+    //   {
+    //     id: '1',
+    //     description: 'Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎',
+    //     url: 'https://www.prisma.io',
+    //   },
+    //   {
+    //     id: '2',
+    //     description: 'The best GraphQL client',
+    //     url: 'https://www.apollographql.com/docs/react/',
+    //   },
+    // ]
 
     return (
       // pass FEED_QUERY as prop to <Query /> Component
       <Query query={FEED_QUERY}>
-        {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching Data</div>
+          if (error) return <div>Error</div>
+          console.log(data);
+
+          const linksToRender = data.feed.links
+          return (
+            <div>
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )
+        }}
       </Query>
     )
   }
